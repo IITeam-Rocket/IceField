3c0517429d2f59ebf792dfbf8a552c6e5fb56cc9
lilla06 <57257557+lilla06@users.noreply.github.com>
2020-03-28 16:18:35 +0100

    Rope: skeleton-print done

--- src/models/items/Rope.java
+++ src/models/items/Rope.java
@@ -3,6 +3,10 @@ package models.items;
 import models.characters.Character;
 import models.policies.HasRopePolicy;
 
+import static controllers.TabController.addIndent;
+import static controllers.TabController.printlnWithIndents;
+import static controllers.TabController.removeIndent;
+
 /**
  * An Item that changes the finding player's
  * RescueFriendPolicy.
@@ -16,6 +20,12 @@ public class Rope extends Item {
      */
     @Override
     public void uponDiscovery(Character finder) {
+        addIndent();
+        printlnWithIndents("ropeDiscovery(finder)");
+
         finder.changeRescuePolicy(new HasRopePolicy());
+
+        printlnWithIndents("return");
+        removeIndent();
     }
 }

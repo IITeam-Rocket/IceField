976cfe35af6a28b503a0f058aecd0eac71eee28e
lilla06 <57257557+lilla06@users.noreply.github.com>
2020-03-28 16:08:52 +0100

    Beacon: skeleton-print done

--- src/models/items/Beacon.java
+++ src/models/items/Beacon.java
@@ -3,6 +3,10 @@ package models.items;
 import models.Environment;
 import models.characters.Character;
 
+import static controllers.TabController.addIndent;
+import static controllers.TabController.printlnWithIndents;
+import static controllers.TabController.removeIndent;
+
 /**
  * A beacon that is one part of the SignalFlare
  * needed to win the game
@@ -15,6 +19,12 @@ public class Beacon extends Item {
      */
     @Override
     public void uponDiscovery(Character finder) {
+        addIndent();
+        printlnWithIndents("beaconDiscovery(finder)");
+
         Environment.getInstance().recordBeacon();
+
+        printlnWithIndents("return");
+        removeIndent();
     }
 }

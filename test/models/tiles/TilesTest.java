c6fa101f7637219c9d3caf63a7427405d32031e7
DAbraham2 <d.abraham96@icloud.com>
2020-03-28 10:58:57 +0100

    JUnitka

--- test/models/tiles/TilesTest.java
+++ test/models/tiles/TilesTest.java
@@ -1,16 +1,42 @@
 package models.tiles;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TilesTest {
-    @BeforeEach
-    void Initialization() {
+import java.util.Random;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
+public class TilesTest {
+
+    @Test
+    void holeInitTest() {
+        Tile tile = new Hole();
+
+        assertNotEquals(0, tile.snowDepth, "Chill out dude");
     }
 
     @Test
-    void Test1(){
+    void noSpaceTest() {
+        Tile tile = new Hole();
+
+        assertEquals(0, tile.getCapacity(), "There room for everyone!!!4");
+    }
+
+    @Test
+    void holeInOneTest() {
+        Random r = new Random();
+        for (int i = 0; i < 100; i++) {
+            Tile t = new Hole();
+            int cocain = t.getSnowDepth();
+            int s = r.nextInt();
+            t.addSnow(s);
+
+            assertEquals(cocain + s, t.getSnowDepth(), "Snowing heavy right?");
+        }
+
 
     }
+
 }

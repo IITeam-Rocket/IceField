990bd6e5e6e07d831d9a7866ca98a4e7a21fc280
tophyan1 <topyan99@gmail.com>
2020-03-28 16:22:55 +0100

    Added MoveCharacterToStable use case
    Signed-off-by: tophyan1 <topyan99@gmail.com>

--- src/controllers/UseCaseExecutor.java
+++ src/controllers/UseCaseExecutor.java
@@ -1,7 +1,21 @@
 package controllers;
 
+import models.characters.Researcher;
+import models.tiles.StableIcePatch;
+
 /**
  * A class that sets up and executes use cases
  */
 public class UseCaseExecutor {
+
+    /**
+     * Sets up and executes the MoveCharacterToStable use case
+     */
+    public static void MoveCharacterToStable() {
+        StableIcePatch source = new StableIcePatch();
+        StableIcePatch dest = new StableIcePatch();
+        Researcher explorer = new Researcher(source);
+
+        explorer.moveTo(dest);
+    }
 }

5dcf6e5aaa4c228ca0a4b1aae65e0e2a587c6ea0
DAbraham2 <d.abraham96@icloud.com>
2020-03-28 07:36:11 +0100

    Mens not hot!

--- test/models/characters/CharactesTest.java
+++ test/models/characters/CharactesTest.java
@@ -1,10 +1,28 @@
 package models.characters;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CharactesTest {
+    Character eskimo;
+    Character researcher;
     @BeforeAll
     void Inicialization(){
-
+        eskimo = new Eskimo();
+        researcher = new Researcher();
     }
+
+    @Test
+    void EskimoDefaultBodyHeatTest() {
+        assertEquals(5, eskimo.bodyHeat, "Wong do not hot");
+    }
+
+    @Test
+    void EskimoBodyHeatGetterTest() {
+        assertEquals(eskimo.bodyHeat, eskimo.getBodyHeat(), "Wong never how");
+    }
+
+
 }

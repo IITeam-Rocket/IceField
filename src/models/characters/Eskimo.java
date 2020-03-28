e6e16f0cbb0cb26efad7fe98d5bc9b2824e31990
lilla06 <57257557+lilla06@users.noreply.github.com>
2020-03-28 16:04:15 +0100

    Eskimo: skeleton-print done

--- src/models/characters/Eskimo.java
+++ src/models/characters/Eskimo.java
@@ -4,6 +4,10 @@ import models.policies.IgluPolicy;
 import models.tiles.IcePatch;
 import models.tiles.Tile;
 
+import static controllers.TabController.addIndent;
+import static controllers.TabController.printlnWithIndents;
+import static controllers.TabController.removeIndent;
+
 /**
  * A character with the ability to build Igloos
  */
@@ -21,7 +25,13 @@ public class Eskimo extends Character {
      */
     @Override
     public void useSpecial(Tile target) {
+        addIndent();
+        printlnWithIndents("buildingIglu(target)");
+
         if (target == tile)
             ((IcePatch) tile).changeFrostBitePolicy(new IgluPolicy());
+
+        printlnWithIndents("return");
+        removeIndent();
     }
 }

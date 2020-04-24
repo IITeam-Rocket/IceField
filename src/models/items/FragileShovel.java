c761eeb5d3ed4d0cab2388f115c763a57e089135
tophyan1 <topyan99@gmail.com>
2020-04-24 19:23:49 +0200

    todo

--- src/models/items/FragileShovel.java
+++ src/models/items/FragileShovel.java
@@ -3,6 +3,7 @@ package models.items;
 import models.figures.Character;
 import models.policies.FragileShovelPolicy;
 
+// TODO: 2020. 04. 24. javadoc
 public class FragileShovel extends Item {
     /**
      * Changes the strategy of the finder to clear an

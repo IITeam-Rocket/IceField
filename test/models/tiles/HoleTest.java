f50094b96740a32651192106b478bd46b1c49929
DAbraham2 <d.abraham96@icloud.com>
2020-04-24 18:13:46 +0200

    changed holetest

--- test/models/tiles/HoleTest.java
+++ test/models/tiles/HoleTest.java
@@ -1,8 +1,6 @@
 package models.tiles;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,7 +18,6 @@ class HoleTest {
     }
 
     @Test
-    @Order(0)
     public void can_initialize_with_no_arguments() {
         assertNotNull(hole, "Hole did not initialize at all!");
         assertEquals(1, hole.getID(), "ID did not initialize correctly!");
@@ -28,7 +25,6 @@ class HoleTest {
     }
 
     @Test
-    @Order(1)
     public void can_initialize_with_ID() {
         assertNotNull(hole1, "First hole did not initialize at all!");
         assertNotNull(hole2, "Second hole did not initialize at all!");
@@ -40,7 +36,6 @@ class HoleTest {
 
 
     @Test
-    @Order(2)
     public void reveal_causes_isDiscovered_to_be_true() {
         hole.reveal();
         assertTrue(hole.isDiscovered(), "Revealing did not cause the hole to be discovered!");

080116a45b2aff047ea1b788082f31097fef83de
tophyan1 <topyan99@gmail.com>
2020-04-24 18:20:17 +0200

    Added FragileShovel class

---
+++ src/models/items/FragileShovel.java
@@ -0,0 +1,16 @@
+package models.items;
+
+import models.figures.Character;
+
+public class FragileShovel extends Item {
+    /**
+     * Changes the strategy of the finder to clear an
+     * ice patch.
+     *
+     * @param finder the character who discovered the item
+     */
+    @Override
+    public void uponDiscovery(Character finder) {
+        // TODO: 2020. 04. 24. implement
+    }
+}

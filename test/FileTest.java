7279899decb596148c33a443f62878677bf305d2
DAbraham2 <d.abraham96@icloud.com>
2020-04-24 22:25:50 +0200

    hotfix's hotfix

--- test/FileTest.java
+++ test/FileTest.java
@@ -2,6 +2,7 @@ import controllers.Prototype;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import testenv.hackOStream;
 
 import java.io.*;
 import java.util.Map;

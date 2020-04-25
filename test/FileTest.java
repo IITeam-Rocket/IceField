54d5e8971187e0998711cf9729b62fab2a5dbf97
DAbraham2 <d.abraham96@icloud.com>
2020-04-25 02:30:08 +0200

    Doin parameterized

--- test/FileTest.java
+++ test/FileTest.java
@@ -2,15 +2,13 @@ import controllers.Prototype;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import testenv.IntArgumentsProvider;
+import testenv.FileMapArgumentsProvider;
 import testenv.hackOStream;
 
 import java.io.*;
 import java.util.Map;
-import java.util.TreeMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,94 +17,40 @@ public class FileTest {
     private final hackOStream consoleOut = new hackOStream();
     private final PrintStream os = System.out;
     private final InputStream is = System.in;
-    private File wd;
-    private Map<File, File> testEnv;
+    private Prototype game;
 
     FileTest() throws IOException {
     }
 
     @BeforeEach
     public void setup() {
-        testEnv = new TreeMap<File, File>();
-        wd = new File(System.clearProperty("user.dir"));
-        File[] files = wd.listFiles();
-
-        for (File f :
-                files) {
-            if (f.isDirectory() && f.getName().equals("test")) {
-                wd = f;
-                break;
-            }
-        }
-        files = wd.listFiles();
-
-        for (File f :
-                files) {
-            if (f.isDirectory() && f.getName().equals("resources")) {
-                wd = f;
-                break;
-            }
-        }
-        FileFilter testFilter = new FileFilter() {
-            @Override
-            public boolean accept(File f) {
-                return f.getName().endsWith("Test");
-            }
-        };
-
-        FileFilter expectedFilter = new FileFilter() {
-            @Override
-            public boolean accept(File f) {
-                return f.getName().endsWith("Expected");
-            }
-        };
-
-        File[] testfiles = wd.listFiles(testFilter);
-        File[] expected = wd.listFiles(expectedFilter);
-
-        for (File f : testfiles) {
-            for (File v : expected) {
-                if (v.getName().startsWith(f.getName())) {
-                    testEnv.put(f, v);
-                }
-            }
-        }
+        game = new Prototype(true);
         System.setOut(consoleOut);
     }
 
+    @ParameterizedTest(name = "{index} => {0}")
+    @ArgumentsSource(FileMapArgumentsProvider.class)
+    public void tester(Map.Entry<File, File> entry) throws IOException {
+        File testInputFile = entry.getKey();
+        File testExpectedOutput = entry.getValue();
+        String testFilename = testInputFile.getName();
+        String testExpectedFilename = testExpectedOutput.getName();
 
-    @ParameterizedTest
-    @ArgumentsSource(IntArgumentsProvider.class)
-    void test(int arg) {
-        System.out.println(arg);
-    }
+        FileInputStream fis = new FileInputStream(testInputFile);
+        System.setIn(fis);
 
-    @Test
-    public void tester() throws IOException, FileTestFailedException {
-        for (Map.Entry<File, File> entry : testEnv.entrySet()) {
+        game.run();
+        FileInputStream efis = new FileInputStream(testExpectedOutput);
+        InputStreamReader isr = new InputStreamReader(efis);
+        BufferedReader br = new BufferedReader(isr);
 
-            File testInputFile = entry.getKey();
-            File testExpectedOutput = entry.getValue();
-            String testFilename = testInputFile.getName();
-            String testExpectedFilename = testExpectedOutput.getName();
+        while (true) {
+            String expectedLine = br.readLine();
+            String actualLine = consoleOut.ReadLine();
 
-            FileInputStream fis = new FileInputStream(testInputFile);
-            System.setIn(fis);
+            if (expectedLine == null) break;
 
-            Prototype game = new Prototype();
-            game.run();
-            FileInputStream efis = new FileInputStream(testExpectedOutput);
-            InputStreamReader isr = new InputStreamReader(efis);
-            BufferedReader br = new BufferedReader(isr);
-
-            while (true) {
-                String expectedLine = br.readLine();
-                String actualLine = consoleOut.ReadLine();
-
-                if (expectedLine == null) break;
-
-                assertEquals(expectedLine, actualLine, String.format("Error in testfile: %s expected output file: %s", testFilename, testExpectedFilename));
-            }
+            assertEquals(expectedLine, actualLine, String.format("Error in testfile: %s expected output file: %s", testFilename, testExpectedFilename));
         }
     }
 

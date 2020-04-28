56068250f5e0a0f95da6ec94549961360e9a7ac7
tophyan1 <topyan99@gmail.com>
2020-04-28 21:58:29 +0200

    Added Observer and Subject for Observer pattern view implementation

---
+++ src/models/Observer.java
@@ -0,0 +1,17 @@
+package models;
+
+/**
+ * An interface representing an object that observes an other object.
+ *
+ * @author Józsa György
+ * @version 1.0
+ * @since graphical
+ * @since 2020.04.28.
+ */
+public interface Observer {
+
+    /**
+     * Updates the object.
+     */
+    void update();
+}
---
+++ src/models/Subject.java
@@ -0,0 +1,51 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A class representing an observed entity.
+ *
+ * @author Józsa György
+ * @version 3.0
+ * @since graphical
+ * @since 2020.04.28.
+ */
+public class Subject {
+    /**
+     * List of observing objects to notify on change.
+     */
+    private List<Observer> observers = new ArrayList<>();
+
+    /**
+     * Registers a new Observer object to notify upon change.
+     *
+     * @param o the new object
+     */
+    public void register(Observer o) {
+        observers.add(o);
+    }
+
+    /**
+     * Notifies all recorded observers that a change has been made.
+     */
+    public void notifyObservers() {
+        observers.forEach(Observer::update);
+    }
+
+    /**
+     * Removes a given Observer object from the list to notify upon change.
+     *
+     * @param o the object to remove
+     */
+    public void remove(Observer o) {
+        observers.remove(o);
+    }
+
+    /**
+     * Clears the list of observers to notify upon change.
+     */
+    public void clear() {
+        observers = new ArrayList<>();
+    }
+}

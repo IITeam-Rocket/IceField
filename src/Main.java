35ce2b1dc66b439c11e56b2a65cf3c2e66ac0754
kovacsmarcell <kovacs.marcell99@gmail.com>
2020-05-17 13:50:38 +0200

    Read map fix

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import controllers.game.Game;
 import controllers.game.GameJFrame;
 
 public class Main {
 
   public static void main(String[] args) {
       GameJFrame frame = GameJFrame.getInstance();
+      Game game = new Game();
+      game.playGame();
     }
 }

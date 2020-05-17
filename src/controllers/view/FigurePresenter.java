c8caea7a83e02983decd757c95ecbfd6e5a86375
tophyan1 <topyan99@gmail.com>
2020-05-18 01:28:40 +0200

    Removed unused imports

--- src/controllers/view/FigurePresenter.java
+++ src/controllers/view/FigurePresenter.java
@@ -1,8 +1,6 @@
 package controllers.view;
 
-import controllers.game.GameJFrame;
 import models.figures.Figure;
-import models.tiles.Tile;
 
 import javax.swing.*;
 import java.io.Serializable;
@@ -10,18 +8,21 @@ import java.io.Serializable;
 public abstract class FigurePresenter implements Serializable {
     /**
      * Get Figure
+     *
      * @return Figure
      */
     public abstract Figure getFigure();
 
     /**
      * Get Image
+     *
      * @return ImageIcon
      */
     public abstract ImageIcon getImage();
 
     /**
      * Draw
+     *
      * @param x where X
      * @param y where Y
      */
